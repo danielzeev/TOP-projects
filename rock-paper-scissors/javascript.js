ed6ed8b8bc1c3bb32ccadaabf0cb84a44166206e
Daniel Zeev <86726359+danielzeev@users.noreply.github.com>
2024-08-12 12:01:57 +0300

    Added  playGame() function with contents of playRound() moved within

--- rock-paper-scissors/javascript.js
+++ rock-paper-scissors/javascript.js
@@ -1,5 +1,5 @@
 // Check the JS is linked to html
-console.log("Hello World")
+// console.log("Hello World")
 
 // Computer selection 
 function getComputerChoice() {
@@ -7,44 +7,58 @@ function getComputerChoice() {
     let idx = Math.floor(Math.random() * 3);
     return rps[idx]
 }
-// console.log(getComputerChoice())
 
 // Human selection 
 function getHumanChoice() {
     return prompt("Enter rock, paper, or scissors").toLowerCase()
 }
-// console.log(getHumanChoice())
 
-// Initial scores
-let humanScore = 0;
-let computerScore = 0;
 
-// Single round of play
-function playRound(humanChoice, computerChoice) {
-    const vectors = {
-        'rock'     : [0,   2],
-        'paper'    : [-2, -2],
-        'scissors' : [2,  -1],
-    };
+function playGame() {
+
+    // Single round of play
+    function playRound(humanChoice, computerChoice) {
+        const outcomes = {
+            'win'  : `You win! ${humanChoice} beats ${computerChoice}`,
+            'lose' : `You lose! ${computerChoice} beats ${humanChoice}`,
+            'tie'  : `Tie! ${computerChoice} is the same as ${humanChoice}`,
+        };  
+        const vectors = {
+            'rock'     : [0,   2],
+            'paper'    : [-2, -2],
+            'scissors' : [2,  -1],
+        };
+        
+        // Calculate cross product
+        let humanVector    = vectors[humanChoice]
+        let computerVector = vectors[computerChoice]    
+        let crossProduct = (humanVector[0] * computerVector[1]) - (humanVector[1] * computerVector[0])
+        
+        // Using cross product to determine winner
+        if (crossProduct < 0) {
+            console.log(outcomes['win']);
+            humanScore += 1;
+        } else if (crossProduct > 0) {
+            console.log(outcomes['lose']);
+            computerScore += 1;       
+        } else {
+            console.log(outcomes['tie']);
+        }
+    }   
     
-    // Calculate cross product
-    let humanVector    = vectors[humanChoice]
-    let computerVector = vectors[computerChoice]    
-    let crossProduct = (humanVector[0] * computerVector[1]) - (humanVector[1] * computerVector[0])
+    // Scores
+    let humanScore = 0;
+    let computerScore = 0;    
     
-    // Using cross product to check for clockwise
-    if (crossProduct < 0) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore += 1;
-    } else if (crossProduct > 0) {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore += 1       
-    } else {
-        console.log(`Tie! ${computerChoice} is the same as ${humanChoice}`);
+    // Play for 5 rounds
+    for (let i = 0; i < 5; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);        
     }
+    return [humanScore, computerScore]
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection)
+let scores = playGame()
+let humanScore = scores[0]
+let computerScore = scores[1]
